ee2a2200990c85b0254f7d348a898abf0b02a481
danieltprice <10074684+danieltprice@users.noreply.github.com>
2023-10-03 14:58:16 -0300

    Update application.ex

--- lib/friends/application.ex
+++ lib/friends/application.ex
@@ -8,8 +8,7 @@ defmodule Friends.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Friends.Worker.start_link(arg)
-      # {Friends.Worker, arg}
+      Friends.Repo,
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
